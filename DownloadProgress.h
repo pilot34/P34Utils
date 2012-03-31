61471900489ed3fe37cbbaaf508130a164aff247
Gleb Tarasov <gleb34@gmail.com>
2012-03-31 05:44:28 +0400

    speed

---
+++ DownloadProgress.h
@@ -0,0 +1,13 @@
+//
+//  DownloadProgress.h
+//  abookclub
+//
+//  Created by Глеб Тарасов on 28.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadProgress : NSObject
+
+@end
---
+++ DownloadProgress.m
@@ -0,0 +1,13 @@
+//
+//  DownloadProgress.m
+//  abookclub
+//
+//  Created by Глеб Тарасов on 28.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DownloadProgress.h"
+
+@implementation DownloadProgress
+
+@end
