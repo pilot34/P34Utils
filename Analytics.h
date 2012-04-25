d7fedc57bd999e594b5b9d91bdfc259d09e3ff78
@gtarasov <@gleb34@gmail.com>
2012-04-26 01:49:30 +0400

    analytics

---
+++ Analytics.h
@@ -0,0 +1,13 @@
+//
+//  Analytics.h
+//  Intuit
+//
+//  Created by Глеб Тарасов on 25.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Analytics : NSObject
+
+@end
---
+++ Analytics.m
@@ -0,0 +1,13 @@
+//
+//  Analytics.m
+//  Intuit
+//
+//  Created by Глеб Тарасов on 25.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Analytics.h"
+
+@implementation Analytics
+
+@end
