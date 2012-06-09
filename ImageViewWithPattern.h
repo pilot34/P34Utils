56fb971fb70c302a64ce5d70206ff4abe0cb8dce
gtarasov <gleb34@gmail.com>
2012-06-10 02:23:25 +0400

    imageview, doAfter

---
+++ ImageViewWithPattern.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewWithPattern.h
+//  Intuit
+//
+//  Created by Глеб Тарасов on 31.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewWithPattern : UIImageView
+
+@end
