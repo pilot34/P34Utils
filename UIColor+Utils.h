9f4ad4a5ecac7ff31600e08e7eef9c4b667d8ba8
gtarasov <gleb34@gmail.com>
2012-07-09 04:45:10 +0400

    color

---
+++ UIColor+Utils.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Utils.h
+//  AldoCoppola
+//
+//  Created by Глеб Тарасов on 06.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Utils)
+
+@end
---
+++ UIColor+Utils.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Utils.m
+//  AldoCoppola
+//
+//  Created by Глеб Тарасов on 06.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIColor+Utils.h"
+
+@implementation UIColor (Utils)
+
+@end
