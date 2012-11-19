9595cdd4fc04ab7a3dfd59e3627cdeef90a01e4d
gtarasov <gleb34@gmail.com>
2012-11-19 17:40:05 +0400

    wself

--- P34Utils.h
+++ P34Utils.h
@@ -81,3 +81,5 @@ void doAfter(CGFloat delay, BasicBlock action);
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#define WSELF           __weak typeof(self) wself = self
