7ed3fa7baa56ff0289ea10dd371962d0ba0f3848
gtarasov <gleb34@gmail.com>
2014-11-12 00:50:12 +0300

    doSyncOnMain

--- P34Utils.m
+++ P34Utils.m
@@ -5,12 +5,20 @@ void doAfter(CGFloat delay, BasicBlock action)
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC), dispatch_get_main_queue(), action);
 }
 
+void doSyncOnMain(BasicBlock action)
+{
+    if ([NSThread isMainThread])
+        action();
+    else
+        dispatch_sync(dispatch_get_main_queue(), action);
+}
+
 BOOL isIphone5OrLarger()
 {
     static NSInteger isIphone5 = -1;
     if (isIphone5 < 0)
     {
-        dispatch_sync(dispatch_get_main_queue(), ^{
+        doSyncOnMain(^{
             isIphone5 = [[UIScreen mainScreen] bounds].size.height >= 568;
         });
     }
@@ -23,7 +31,7 @@ BOOL isIphone5()
     static NSInteger isIphone5 = -1;
     if (isIphone5 < 0)
     {
-        dispatch_sync(dispatch_get_main_queue(), ^{
+        doSyncOnMain(^{
             isIphone5 = [[UIScreen mainScreen] bounds].size.height == 568;
         });
     }
@@ -36,7 +44,7 @@ BOOL isIphone6()
     static NSInteger isIphone6 = -1;
     if (isIphone6 < 0)
     {
-        dispatch_sync(dispatch_get_main_queue(), ^{
+        doSyncOnMain(^{
             isIphone6 = [[UIScreen mainScreen] bounds].size.height == 667;
         });
     }
@@ -48,7 +56,7 @@ BOOL isIphone6Plus()
     static NSInteger isIphone6Plus = -1;
     if (isIphone6Plus < 0)
     {
-        dispatch_sync(dispatch_get_main_queue(), ^{
+        doSyncOnMain(^{
             isIphone6Plus = [[UIScreen mainScreen] bounds].size.height == 736;
         });
     }
@@ -60,7 +68,11 @@ BOOL isIpad()
 {
     static NSInteger isiPad = -1;
     if (isiPad < 0)
-        isiPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+    {
+        doSyncOnMain(^{
+            isiPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+        });
+    }
     return isiPad > 0;
 }
 
@@ -68,7 +80,10 @@ BOOL isRetina()
 {
     static NSInteger isRetina = -1;
     if (isRetina < 0)
+    {   doSyncOnMain(^{
         isRetina = UIScreen.mainScreen.scale > 1;
+    });
+    }
     
     return isRetina > 0;
 }
