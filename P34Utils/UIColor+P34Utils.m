33e6492fa27217314e0d744a54c54dc01a01b530
gtarasov <gleb34@gmail.com>
2015-11-14 01:13:13 +0300

    removed log

--- P34Utils/UIColor+P34Utils.m
+++ P34Utils/UIColor+P34Utils.m
@@ -31,7 +31,6 @@
     igreen = roundf(green * 255);
     iblue = roundf(blue * 255);
     NSUInteger result = (ired << 16) | (igreen << 8) | iblue;
-    NSLog(@"hex %lX", (unsigned long)result);
     return result;
 }
 
