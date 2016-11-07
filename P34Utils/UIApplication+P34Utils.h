9e0be90a4445e45e7daf4ec177d634065b7d686f
Gleb <gleb34@gmail.com>
2016-11-08 02:33:53 +0300

    Update UIApplication+P34Utils.h

--- P34Utils/UIApplication+P34Utils.h
+++ P34Utils/UIApplication+P34Utils.h
@@ -8,9 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-NS_EXTENSION_UNAVAILABLE_IOS("Disabled for app extensions.")
 @interface UIApplication (Utils)
 
-+ (BOOL)landscape;
++ (BOOL)landscape NS_EXTENSION_UNAVAILABLE_IOS("Disabled for app extensions.");
 
 @end
